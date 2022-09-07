b7843c3884939425241081276650880877106f82
RichardJoan <110168588+RichardJoan@users.noreply.github.com>
2022-09-07 17:46:51 +0530

    Add files via upload

---
+++ HomePage.java
@@ -0,0 +1,19 @@
+package com.leaftaps.pages;
+
+import com.framework.selenium.api.design.Locators;
+import com.framework.testng.api.base.ProjectSpecificMethods;
+
+public class HomePage extends ProjectSpecificMethods{
+	
+	public SelectPage verifyHomePage() {
+		verifyDisplayed(locateElement(Locators.CLASS_NAME, "uiOutputText"));
+		reportStep("Homepage is loaded", "pass");
+		return new SelectPage();
+	}
+	
+	/*
+	 * public MyHomePage clickCrmsfaLink() { click(locateElement(Locators.LINK_TEXT,
+	 * "CRM/SFA")); reportStep("CRM/SFA link is clicked", "pass"); return new
+	 * MyHomePage(); }
+	 */
+}
